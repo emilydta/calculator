2ce9cee4c898f15de394b62557411d8b1fda6e8e
Emily <emilydta@users.noreply.github.com>
2021-12-18 15:40:12 +1100

    added basic functionality for equals and addition

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const display = document.getElementById("display");
+//const total = document.getElementById("total");
 const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
@@ -16,13 +17,35 @@ const zero = document.getElementById("zero");
 const clear = document.getElementById("clear");
 const divide = document.getElementById("divide");
 
-let calculatorDisplayValue = "";
-display.innerText = calculatorDisplayValue;
 
-function operate(operator, firstNumber, secondNumber) {}
 
-function addButton(firstNumber, secondNumber) {
-    return console.log(firstNumber+secondNumber);
+let firstNumber = "";
+let secondNumber = "";
+let operator = "blank";
+let total = 0;
+
+
+function operate() {
+    if (operator === "+") {
+        total = parseInt(firstNumber) + parseInt(secondNumber);
+        operator = "blank";
+        firstNumber = total;
+        secondNumber = "";
+        return display.innerText = total;
+    } else if (operator === "-") {
+        return subtractButton(firstNumber, secondNumber);
+    }
+    return;
+}
+
+
+function addButton() {
+    if (secondNumber != true) {
+        operator = "+";
+        return display.innerText = `${firstNumber} ${operator}`;
+    } else 
+        total = parseInt(firstNumber) + parseInt(secondNumber);
+        return display.innerText = total;
 }
 
 function subtractButton(firstNumber, secondNumber) {
@@ -38,27 +61,36 @@ function divideButton(firstNumber, secondNumber) {
 }
 
 function clearButton() {
-    calculatorDisplayValue = "";
-    display.innerText = calculatorDisplayValue;
+    firstNumber = "";
+    secondNumber = "";
+    display.innerText = firstNumber + secondNumber;
 }
 
-function sevenButton(number) {
-    number = 7;
-    console.log(number);
-    calculatorDisplayValue = calculatorDisplayValue + "7";
-    display.innerText = calculatorDisplayValue;
+function sevenButton() {
+    if (operator == "blank") {
+       firstNumber = firstNumber + "7";
+       display.innerText = firstNumber;
+    } else if (operator !== "blank") {
+        secondNumber = secondNumber + "7";
+        return display.innerText = `${firstNumber} ${operator} ${secondNumber}`;
+    }
 }
 
-function eightButton(number) {
-    number = 8;
-    console.log(number);
-    calculatorDisplayValue = calculatorDisplayValue + "8";
-    display.innerText = calculatorDisplayValue;
+function eightButton() {
+    if (operator == "blank") {
+        firstNumber = firstNumber + "8";
+        display.innerText = firstNumber;
+     } else if (operator !== "blank") {
+         secondNumber = secondNumber + "8";
+         return display.innerText = `${firstNumber} ${operator} ${secondNumber}`;
+     }
 }
 
 seven.addEventListener("click", () => sevenButton());
 eight.addEventListener("click", () => eightButton());
 add.addEventListener("click", () => addButton());
 clear.addEventListener("click", () => clearButton());
+equals.addEventListener("click", () => operate());
 
 
+    
